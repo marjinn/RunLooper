7272c0ddc3efc232d18a02b923fbaf384c2f7a36
mar Jinn <marjinn@mars-Mac.local>
2014-07-22 06:16:43 +0530

    MethodInvocation

--- RunLooper/main.m
+++ RunLooper/main.m
@@ -55,6 +55,66 @@
 
 int main(int argc, char * argv[])
 {
+    
+    /**
+     Invoke UIAlertView
+     */
+    //"UIAlertController"
+    Class UIAlertController_class = nil;
+    UIAlertController_class =
+    NSClassFromString(@"UIAlertController");
+    
+//    + (instancetype)alertControllerWithTitle:(NSString *)title message:(NSString *)message preferredStyle:(UIAlertControllerStyle)preferredStyle;
+
+    SEL alertController = nil;
+    alertController = @selector(alertControllerWithTitle:message:preferredStyle:);
+    //NSSelectorFromString(@"alertControllerWithTitle:message:preferredStyle:");
+    
+    //alertController =  @selector(viewControllerWithRestorationIdentifierPath:coder:);
+    
+    NSMethodSignature* methodSign = nil;
+    const char returnValEncode[2] = @encode(id);
+    const char firstParamEncode[2] = @encode(id);
+    const char secondParamEncode[2] = @encode(SEL);
+    const char thirdParamEncode[2] = @encode(NSString*);
+    const char fourthParamEncode[2] = @encode(NSString*);
+    const char fifthParamEncode[2] = @encode(int);
+    
+    NSString* _string = nil;
+    _string =
+    [NSString stringWithFormat:@"%s%s%s%s%s%s",returnValEncode,firstParamEncode,secondParamEncode,thirdParamEncode,fourthParamEncode,fifthParamEncode];
+    
+    const char* types = NULL;
+    types = [_string cStringUsingEncoding:NSUTF8StringEncoding];
+    
+    
+   //methodSign = [[NSMethodSignature new ]methodSignatureForSelector:alertController];
+   //methodSign = [NSMethodSignature signatureWithObjCTypes:"@@:@@i"];
+    methodSign = [NSMethodSignature signatureWithObjCTypes:types];
+    
+    NSInvocation* invokeAlertController = nil;
+    invokeAlertController =
+    [NSInvocation invocationWithMethodSignature:methodSign];
+    //invokeAlertController = [[NSInvocation alloc]init];
+    
+    
+    [invokeAlertController setSelector:alertController];
+    
+    NSString* title = @"Blaah";
+    NSString* message = @"Blaah";
+    int prefferedStyle = 0;
+    
+    [invokeAlertController setArgument:(void *)&title atIndex:(NSInteger)2];
+    [invokeAlertController setArgument:(void *)&message atIndex:(NSInteger)3];
+    [invokeAlertController setArgument:(void *)&prefferedStyle atIndex:(NSInteger)4];
+    
+   
+    [invokeAlertController invokeWithTarget:[NSClassFromString(@"UIAlertController") class]];
+    
+    id returnV = nil;
+    [invokeAlertController getReturnValue:(void *)&returnV];
+    
+    
     TrialAndErrorClass* trial = nil;
     trial = [TrialAndErrorClass new];
     
